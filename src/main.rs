a3e5e7708e5d6d63e5cb9d9f4bf55970fbe370e9
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-07-11 09:43:38 -0600

    Initial Commit via GitAuto

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,63 @@
 /* Written by Juan Pablo Gutiérrez */
 
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+const width: u32 = 20;
+const height: u32 = 60;
+
 fn main() {
-    println!("Hello, world!");
+    let score: u32 = 0;
+
+    let a: [i32; 4] = [1, 2, 3, 4];
+   /*  let w : usize = width.parse();
+    let b: [[u32; width]; height] = [[2123], [123]]; */
+    // Game Loop
+    loop {}
+}
+
+fn screen() {
+    
+    let x : usize;
+    for i in 0..=height {
+        println!("");
+        print!(" | ");
+        for j in 0..=width {
+            print!(" . ");
+        }
+        print!(" | ");
+    }
+}
+
+fn guessing() {
+    println!("Welcome to the guessing game!!");
+
+    let secret_num: u32 = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Guess the number");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
